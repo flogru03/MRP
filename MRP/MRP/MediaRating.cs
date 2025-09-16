f73de57da9cff37cd98638de29c0a84c1bb7c167
Florian Gruber <florian_03@gmx.at>
2025-09-16 16:06:17 +0200

    refactoring MediaRating

--- MRP/MRP/MediaRating.cs
+++ MRP/MRP/MediaRating.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,61 +7,34 @@ using System.Threading.Tasks;
 
 namespace MRP
 {
-    internal struct MediaRatingData
-    {
-        public uint Value;
-        public string Comment;
-
-        public MediaRatingData()
-        {
-            Value = 0;
-            Comment = string.Empty;
-        }
-        public MediaRatingData(uint value, string comment)
-        {
-            Value = value; 
-            Comment = comment;
-        }
-    }
     internal class MediaRating
     {
-        private User _creator;
-        private MediaEntry _entry;
-
-        private MediaRatingData _data;
-        private DateTime _timestamp;
-        private uint _likes;
-        private bool _isPublic;
-        public uint Value
-        {
-            get 
-            { 
-                return _data.Value; 
-            }
-            set 
-            {
-                _data.Value = (value < 0 || value > 5) ? value : throw new Exception("Value out of Bounds"); 
-            }
-        }
-        public bool IsPublic 
-        {
-            get 
-            {  
-                return _isPublic; 
-            }
-            set 
-            { 
-                _isPublic = value; 
-            }
-        }
+        public User Creator { get; private set; }
+        public MediaEntry Entry { get; private set; }
+        public uint Value { get; private set; }
+        public bool IsPublic { get; private set; }
+        public string Comment { get; private set; } 
+        public DateTime CreatedAt {  get; private set; }
+        public uint Likes { get; private set; }
         public MediaRating(MediaEntry entry, User user)
         {
-            _data = new MediaRatingData();
-            _entry = entry;
-            _creator = user;
-            _timestamp = DateTime.Now;
-            _likes = 0;
+            Entry = entry;
+            Creator = user;
 
+            Value = 0;
+            IsPublic = false;
+            Comment = string.Empty;
+            CreatedAt = DateTime.Now;
+            Likes = 0;
+        }
+        public void SetPublic(bool isPublic)
+        {
+            if (isPublic == IsPublic)
+            {
+                Console.WriteLine("No Changes made...");
+                return;
+            }
+            IsPublic = isPublic;
         }
     }
 }

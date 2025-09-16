d38aba2c6b4237168eb21dd15d72ff1f994bbe51
Florian Gruber <florian_03@gmx.at>
2025-09-16 11:40:44 +0200

    fix file structure

---
+++ MRP/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRP
+{
+    internal class User
+    {
+        private readonly string _username;
+        private readonly string _password;
+        private List<MediaRating> _ratings;
+        private List<MediaEntry> _entries;
+        private List<MediaEntry> _favorites;
+
+        public string Username 
+        { 
+            get => _username; 
+        }
+        public uint NumRatings 
+        {
+            get => (uint) _ratings.Count;
+        }
+        public uint CommonRating 
+        {
+            get
+            {
+                uint total = 0;
+                foreach (var rating in _ratings)
+                {
+                    total += rating.Value;
+                }
+                return total > 0 ? (uint) _ratings.Count / total : 0;
+            }
+        }
+
+        public User(string username, string password)
+        {
+            _username = username;
+            _password = password;
+            _ratings = new List<MediaRating>();
+            _entries = new List<MediaEntry>();
+            _favorites = new List<MediaEntry>();
+        }
+    }
+}
